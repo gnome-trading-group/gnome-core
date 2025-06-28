ad13ec6e1d28bef0f4924e7ea1528446578f3f78
mprey <masonprey7@gmail.com>
2025-06-28 12:18:43 -0400

    fix: remove length and offset values

--- src/main/java/group/gnometrading/disruptor/SBEWrapper.java
+++ src/main/java/group/gnometrading/disruptor/SBEWrapper.java
@@ -5,12 +5,8 @@ import org.agrona.MutableDirectBuffer;
 public class SBEWrapper {
 
     public MutableDirectBuffer buffer;
-    public int offset;
-    public int length;
 
-    public void update(final MutableDirectBuffer buffer, final int offset, final int length) {
+    public void update(final MutableDirectBuffer buffer) {
         this.buffer = buffer;
-        this.offset = offset;
-        this.length = length;
     }
 }
