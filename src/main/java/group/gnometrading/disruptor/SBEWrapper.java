6e8e377f97e3d02f99b3d7bb6c7418b7ab748260
mprey <masonprey7@gmail.com>
2025-06-28 12:21:03 -0400

    Revert "fix: remove length and offset values"
    This reverts commit ad13ec6e1d28bef0f4924e7ea1528446578f3f78.

--- src/main/java/group/gnometrading/disruptor/SBEWrapper.java
+++ src/main/java/group/gnometrading/disruptor/SBEWrapper.java
@@ -5,8 +5,12 @@ import org.agrona.MutableDirectBuffer;
 public class SBEWrapper {
 
     public MutableDirectBuffer buffer;
+    public int offset;
+    public int length;
 
-    public void update(final MutableDirectBuffer buffer) {
+    public void update(final MutableDirectBuffer buffer, final int offset, final int length) {
         this.buffer = buffer;
+        this.offset = offset;
+        this.length = length;
     }
 }
